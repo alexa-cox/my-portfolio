9062948ec23ea4a73a624c8516cbac5babc525bb
Alexandria Cox <alexacox.atc@gmail.com>
2024-12-19 06:31:19 -0500

    build: Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/main.scss';
+import Sidebar from './components/Sidebar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Sidebar />
+  </>
+);
